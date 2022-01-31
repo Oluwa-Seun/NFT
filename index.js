bfda20f74fd7ceab07244164aae4a19d8227150c
JujuBug <61301594+Oluwa-Seun@users.noreply.github.com>
2022-01-31 10:41:41 +0100

    function added

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
         <!-- bg -->
         <!-- head -->
-        <!-- hair -->
         <!-- eyes -->
-        <!-- nose -->
+        <!-- teath -->
         <!-- mouth -->
-        <!-- beard -->
     </svg>
 `
 
